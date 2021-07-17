6caa23a473716de7c971c1194b4a9c8b45a89346
ATOMIC09 <66838025+ATOMIC09@users.noreply.github.com>
2021-07-17 20:05:05 +0700

    Create bmr_cal.py

---
+++ bmr_cal.py
@@ -0,0 +1,13 @@
+gender = input("Enter Gender M/FM : ")
+weight = int(input("Enter Weight (kg) : "))
+height = int(input("Enter Height (cm) : "))
+age = int(input("Enter Age : "))
+
+bmr_male = 66 + (13.7 * weight) + (5 * height) - (6.8 * age)
+bmr_female = 665 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
+
+if gender == "M" or "m" :
+    print(f"Your calories per day: {bmr_male}")
+
+elif gender == "FM" or "fm" :
+    print(f"Your calories per day : {bmr_female}")
